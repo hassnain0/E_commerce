c72b66937230c7b67fdaea77d99a3dfb85405b1a
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-07-09 17:46:41 +0500

    Login SignUp Screen UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignUp from "./Pages/loginSignUp";
+import Footer from "./Components/Footer/Footer";
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png';
+import kid_banner from './Components/Assets/banner_kids.png';
 
 function App() {
   return (
@@ -15,18 +19,18 @@ function App() {
         <NavBar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="mens" />} />
-          <Route path="/womens" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
           <Route path="product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
-          <Route path='/cart' element={<Cart/>} />
-          <Route path='/login' element={<LoginSignUp/>} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignUp />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
 }
-
 export default App;
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
+import Poplular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers";
+import NewCollection from "../Components/NewCollections/NewCollection";
+import NewsLetter from "../Components/NewLetters/NewsLetters";
 
 const Shop = () => {
   return (
     <div>
-      <Hero></Hero>
+      <Hero />
+      <Poplular />
+      <Offers />
+      <NewCollection />
+      <NewsLetter />
     </div>
   );
 };
